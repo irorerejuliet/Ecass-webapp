bff163c4e65c00529076326a7353bb09614c64ba
irorere juliet <irorerejuliet@gmail.com>
2026-04-09 20:51:01 +0100

    fixing navbar

--- src/components/features/ControlPanel.tsx
+++ src/components/features/ControlPanel.tsx
@@ -2,14 +2,14 @@ import { VolumeX, X } from "lucide-react";
 
 const ControlPanel = () => {
   return (
-    <div className="flex lg:flex-col flex-row gap-4 w-full max-w-3xl text-black font-bold">
-      <button className="flex-1 border-4 border-black py-4 bg-white shadow-md flex items-center justify-center">
+    <div className="flex flex-col lg:flex-row gap-4  text-black font-bold">
+      <button className="flex-1 border-4 border-black py-4 bg-white shadow-md flex items-center justify-center w-full lg:w-[415px]">
         <X color="red" size={24} />
         CANCEL
       </button>
 
       <div className="flex-1 border-4 border-black py-4 bg-white shadow-md flex items-center justify-between px-4">
-        <button className="flex items-center justify-center">
+        <button className="flex items-center justify-center ">
           <VolumeX color="gray" size={24} />
           <span>SILENT TRIGGER</span>
         </button>
